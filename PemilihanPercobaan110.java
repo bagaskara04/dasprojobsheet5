838789faa0523ae96f4a9f45f75f906d2bbf7cfb
karalion9 <sagabdespair01@gmail.com>
2023-10-11 12:43:51 +0700

    commit

--- PemilihanPercobaan110.java
+++ PemilihanPercobaan110.java
@@ -2,11 +2,11 @@
  * PemilihanBilangan10
  */
 import java.util.Scanner;
-public class PemilihanBilangan10 {
+public class PemilihanPercobaan110 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input10 = new Scanner(System.in);
         System.out.println("Masukkan sebuah  angka : ");
-        int angka = input.nextInt();
+        int angka = input10.nextInt();
 
         if (angka % 2 == 0) {
             System.out.println("Angka "+angka+" Termasuk bilangan genap");
