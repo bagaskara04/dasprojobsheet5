e37562f995c73d969cded736cf095285a2515baf
karalion9 <sagabdespair01@gmail.com>
2023-10-13 12:51:26 +0700

    commit

---
+++ PemilihanPercobaan210.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class PemilihanPercobaan210 {
+    public static void main(String[] args) {
+        Scanner input10 = new Scanner(System.in);
+
+        System.out.println("Nilai UAS   : ");
+        float uas = input10.nextFloat();
+        System.out.println("Nilai UTS   : ");
+        float uts = input10.nextFloat();
+        System.out.println("Nilai kuis  : ");
+        float kuis = input10.nextFloat();
+        System.out.println("Nilai tugas : ");
+        float tugas = input10.nextFloat();
+
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+
+        String message = total < 65 ? "Remidi" : "Tidak Remidi";
+        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+
+        if (total >= 80) {
+            System.out.println("Nilai Huruf A, Kualifikasi Sangat Baik");
+        } else if(total >=73) {
+            System.out.println("Nilai huruf B+, Kualifikasi Lebih dari baik");
+        } else if(total >=65){
+            System.out.println("Nilai huruf B, Kualifikasi Baik");
+        } else if(total >=60){
+            System.out.println("Nilai huruf C+, Kualifikasi Lebih dari cukup");
+        } else if(total >=50){
+            System.out.println("Nilai huruf C, Kualifikasi cukup");
+        } else if(total >=39){
+            System.out.println("Nilai huruf D, Kualifikasi kurang");
+        } else{
+            System.out.println("Nilai huruf E, Gagal");
+        }
+    }
+}
+
