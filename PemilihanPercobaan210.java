a63dc50f0ff38e9e69441e32a0bed95190e1e421
karalion9 <sagabdespair01@gmail.com>
2023-10-17 19:12:56 +0700

    commit

--- PemilihanPercobaan210.java
+++ PemilihanPercobaan210.java
@@ -3,19 +3,19 @@ public class PemilihanPercobaan210 {
     public static void main(String[] args) {
         Scanner input10 = new Scanner(System.in);
 
-        System.out.println("Nilai UAS   : ");
+        System.out.print("Nilai UAS   : ");
         float uas = input10.nextFloat();
-        System.out.println("Nilai UTS   : ");
+        System.out.print("Nilai UTS   : ");
         float uts = input10.nextFloat();
-        System.out.println("Nilai kuis  : ");
+        System.out.print("Nilai kuis  : ");
         float kuis = input10.nextFloat();
-        System.out.println("Nilai tugas : ");
+        System.out.print("Nilai tugas : ");
         float tugas = input10.nextFloat();
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
 
         String message = total < 65 ? "Remidi" : "Tidak Remidi";
-        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+        System.out.println("Nilai akhir = " + total + " Sehingga " + message);
 
         if (total >= 80) {
             System.out.println("Nilai Huruf A, Kualifikasi Sangat Baik");
